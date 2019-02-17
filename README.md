c5b4ed4c141347bfbe7cbf1beafd80d53178b5ea
trixobird <george.trichopoulos@gmail.com>
2019-02-17 23:03:11 +0200

    Change Name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MsgClient
+# PumaPayClient
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'msg-client'`, () => {
+  it(`should have as title 'pumapay-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('msg-client');
+    expect(app.title).toEqual('pumapay-client');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to msg-client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pumapay-client!');
   });
 });
